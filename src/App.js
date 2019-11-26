30e267e7ea7a4821d43e6ce51314f83f257d0f05
Gabriel Lima <gabriellfsouza@gmail.com>
2019-11-26 15:24:16 -0300

    estrutura

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Routes from './routes';
+import GlobalStyles from './styles/global';
 
 function App() {
-  return <div />;
+  return (
+    <>
+      <Routes />
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/DetalhesCurso/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function DetalhesCurso() {
+  return (
+    <div>
+      <header>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/JrVS7YsGw8w?controls=0"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </header>
+      <content></content>
+      <footer></footer>
+    </div>
+  );
+}

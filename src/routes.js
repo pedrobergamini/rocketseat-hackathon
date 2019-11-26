1e4c7953ed10fc9157e3642bac3322c50ea9f56c
Pedro Bergamini <pedrohgbergamini@gmail.com>
2019-11-26 17:03:40 -0300

    chore: small fix in routes component

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Levels from './pages/Levels';
+import Courses from './pages/Courses';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/levels" component={Levels} />
+      <Route path="/courses/:level" component={} />
     </Switch>
   );
 }

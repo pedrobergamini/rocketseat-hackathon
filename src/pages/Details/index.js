4d9cf548577b27f6cb8d3497690960f0bfa2aba5
Gabriel Lima <gabriellfsouza@gmail.com>
2019-11-27 11:18:07 -0300

    chore: new details page

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import { Card, CardContent, Typography, CardMedia } from '@material-ui/core';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Details() {
-  return <div />;
+  return (
+    <Container>
+      <div className="image-title">
+        <img src="https://auditoriacidada.org.br/wp-content/uploads/2018/01/curso-1500x575.jpg" />
+      </div>
+      <div className="description">
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="h2">
+              Título do meu curso
+            </Typography>
+            <Typography variant="body1" color="textSecondary" component="p">
+              Lizards are a widespread group of squamate reptiles, with over
+              6,000 species, ranging across all continents except Antarctica
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="aulas">
+        <Typography variant="h8" color="textSecondary" component="p">
+          Total de aulas: 5
+        </Typography>
+        <div className="cards">
+          {/* <div className="card"> */}
+          <Card>
+            <CardMedia
+              style={{
+                height: 0,
+                paddingTop: '10%', // 16:9
+              }}
+              image="https://auditoriacidada.org.br/wp-content/uploads/2018/01/curso-1500x575.jpg"
+              title="Paella dish"
+            />
+            texto
+          </Card>
+          {/* </div> */}
+          {/* <div className="card"> */}
+          <Card>texto</Card>
+          {/* </div> */}
+        </div>
+      </div>
+    </Container>
+  );
 }
---
+++ src/pages/Details/styles.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+// import { Card } from '@material-ui/core';
+
+export const Container = styled.div`
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+
+  .description {
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .aulas {
+    margin: 10px;
+  }
+
+  .image-title {
+    display: flex;
+    overflow: hidden;
+    width: 100%;
+    height: auto;
+    overflow: hidden;
+    border-radius: 4px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      /* margin-left: -50%; */
+    }
+  }
+
+  .aulas {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .cards {
+    display: flex;
+    flex-direction: row;
+
+    > div {
+      display: flex;
+      flex: 1;
+      margin: 10px;
+      padding: 100px auto;
+    }
+  }
+`;

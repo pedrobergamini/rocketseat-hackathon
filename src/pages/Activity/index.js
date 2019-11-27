075b394940d55f2bb5d0bc5dd3ee9beb3e228ea0
Gabriel Lima <gabriellfsouza@gmail.com>
2019-11-27 12:14:16 -0300

    feat: activity sample

---
+++ src/pages/Activity/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+
+import {
+  Card,
+  CardContent,
+  Typography,
+  FormControl,
+  FormLabel,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+} from '@material-ui/core';
+
+import { Container } from './styles';
+
+export default function Activity() {
+  return (
+    <Container>
+      <div className="video-container">
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/UauiAPQb3fs"
+          frameBorder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        />
+      </div>
+      <div className="description">
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="h2">
+              Lorem ipsum
+            </Typography>
+            <Typography variant="body1" color="textSecondary" component="p">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              pellentesque eleifend mauris, ut placerat magna. Vivamus felis
+              eros, malesuada id turpis quis, dictum rhoncus orci. In
+              vestibulum,Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit. Curabitur pellentesque eleifend mauris, ut placerat magna.
+              Vivamus felis eros, malesuada id turpis quis, dictum rhoncus orci.
+              In vestibulum,Lorem ipsum dolor sit amet, consectetur adipiscing
+              elit. Curabitur pellentesque eleifend mauris, ut placerat magna.
+              Vivamus felis eros, malesuada id turpis quis, dictum rhoncus orci.
+              In vestibulum,
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+      <div className="excercises">
+        <div className="exercise">
+          <Card>
+            <CardContent>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">
+                  Curabitur pellentesque eleifend mauris, ut placerat magna.
+                  Vivamus felis eros, malesuada id turpis quis?
+                </FormLabel>
+                <FormGroup>
+                  <FormControlLabel control={<Checkbox />} label="Gilad Gray" />
+                  <FormControlLabel
+                    control={<Checkbox />}
+                    label="Jason Killian"
+                  />
+                  <FormControlLabel
+                    control={<Checkbox />}
+                    label="Antoine Llorca"
+                  />
+                </FormGroup>
+              </FormControl>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </Container>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import Levels from './pages/Levels';
 import Courses from './pages/Courses';
 import Details from './pages/Details';
+import Activity from './pages/Activity';
 
 export default function Routes() {
   return (
@@ -11,6 +12,7 @@ export default function Routes() {
       <Route path="/levels" component={Levels} />
       <Route exact path="/courses/:level" component={Courses} />
       <Route exact path="/details" component={Details} />
+      <Route exact path="/activity" component={Activity} />
     </Switch>
   );
 }

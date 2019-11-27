7654ef73c0a673442e39b279b794066f9c97e8f2
Gabriel Lima <gabriellfsouza@gmail.com>
2019-11-27 11:41:42 -0300

    feature: details page

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, Typography, CardMedia } from '@material-ui/core';
+import { Card, CardContent, Typography } from '@material-ui/core';
 
 import { Container } from './styles';
 
@@ -13,36 +13,104 @@ export default function Details() {
         <Card>
           <CardContent>
             <Typography gutterBottom variant="h6" component="h2">
-              Título do meu curso
+              Lorem ipsum
             </Typography>
             <Typography variant="body1" color="textSecondary" component="p">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+              pellentesque eleifend mauris, ut placerat magna. Vivamus felis
+              eros, malesuada id turpis quis, dictum rhoncus orci. In
+              vestibulum,
             </Typography>
           </CardContent>
         </Card>
       </div>
       <div className="aulas">
         <Typography variant="h8" color="textSecondary" component="p">
-          Total de aulas: 5
+          Total de aulas: 6
         </Typography>
         <div className="cards">
-          {/* <div className="card"> */}
           <Card>
-            <CardMedia
-              style={{
-                height: 0,
-                paddingTop: '10%', // 16:9
-              }}
-              image="https://auditoriacidada.org.br/wp-content/uploads/2018/01/curso-1500x575.jpg"
-              title="Paella dish"
-            />
-            texto
+            <div className="image-title">
+              <img src="https://www1.racgp.org.au/getattachment/460451f5-7e0a-47a8-a1d5-e964b705e062/attachment.aspx" />
+            </div>
+            <CardContent>
+              <Typography gutterBottom variant="h9" component="h4">
+                Aula 1
+              </Typography>
+              <Typography variant="h15" color="textSecondary" component="p">
+                Enviado há 2 dias
+              </Typography>
+            </CardContent>
           </Card>
-          {/* </div> */}
-          {/* <div className="card"> */}
-          <Card>texto</Card>
-          {/* </div> */}
+          <Card>
+            <div className="image-title">
+              <img src="https://www1.racgp.org.au/getattachment/460451f5-7e0a-47a8-a1d5-e964b705e062/attachment.aspx" />
+            </div>
+            <CardContent>
+              <Typography gutterBottom variant="h9" component="h4">
+                Aula 2
+              </Typography>
+              <Typography variant="h15" color="textSecondary" component="p">
+                Enviado há 2 dias
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="cards">
+          <Card>
+            <div className="image-title">
+              <img src="https://www1.racgp.org.au/getattachment/460451f5-7e0a-47a8-a1d5-e964b705e062/attachment.aspx" />
+            </div>
+            <CardContent>
+              <Typography gutterBottom variant="h9" component="h4">
+                Aula 3
+              </Typography>
+              <Typography variant="h15" color="textSecondary" component="p">
+                Enviado há 2 dias
+              </Typography>
+            </CardContent>
+          </Card>
+          <Card>
+            <div className="image-title">
+              <img src="https://www1.racgp.org.au/getattachment/460451f5-7e0a-47a8-a1d5-e964b705e062/attachment.aspx" />
+            </div>
+            <CardContent>
+              <Typography gutterBottom variant="h9" component="h4">
+                Aula 4
+              </Typography>
+              <Typography variant="h15" color="textSecondary" component="p">
+                Enviado há 2 dias
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="cards">
+          <Card>
+            <div className="image-title">
+              <img src="https://www1.racgp.org.au/getattachment/460451f5-7e0a-47a8-a1d5-e964b705e062/attachment.aspx" />
+            </div>
+            <CardContent>
+              <Typography gutterBottom variant="h9" component="h4">
+                Aula 5
+              </Typography>
+              <Typography variant="h15" color="textSecondary" component="p">
+                Enviado há 2 dias
+              </Typography>
+            </CardContent>
+          </Card>
+          <Card>
+            <div className="image-title">
+              <img src="https://www1.racgp.org.au/getattachment/460451f5-7e0a-47a8-a1d5-e964b705e062/attachment.aspx" />
+            </div>
+            <CardContent>
+              <Typography gutterBottom variant="h9" component="h4">
+                Aula 6
+              </Typography>
+              <Typography variant="h15" color="textSecondary" component="p">
+                Enviado há 2 dias
+              </Typography>
+            </CardContent>
+          </Card>
         </div>
       </div>
     </Container>
